08e54fa8e19c8780c5e2289bef5e8d085c3d0b29
barondevke <ndabasteve1@gmail.com>
2023-10-25 15:17:52 +0300

    added sign in and sign up

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const bcrypt = require("bcrypt");
 
 const UserData = require("./Models/UserDataModel");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT = 4000
 
 app.post("/SignIn", async (req, res) => {
     const data = req.body;
@@ -39,6 +41,7 @@ app.post("/SignIn", async (req, res) => {
           UserID: user._id.toString(),
           Name: user.Name,
           Email: user.Email,
+          UserName: user.UserName
         },
         message: `USER SIGNED IN SUCCESSFULLY`,
         proceed: true,
@@ -48,11 +51,48 @@ app.post("/SignIn", async (req, res) => {
     }
   });
 
+  app.post("/SignUp", async (req, res) => {
+    const data = req.body;
+    try {
+      const emailInUse = await UserData.findOne({
+        Email: data.Email,
+      });
+      if (emailInUse) {
+        return res.send({
+          message: `EMAIL ALREADY IN USE`,
+          proceed: false,
+        });
+      }
+      
+  
+      const saltRounds = 12; // increase the number of salt rounds
+      const hashedPassword = await bcrypt.hash(data.Password, saltRounds);
+  
+      delete data.Password;
+      data.HashedPassword = hashedPassword;
+  
+      const user = await UserData.create(data);
+      return res.send({
+        data: {
+          UserID: user._id.toString(),
+          Name: user.Name,
+          Email: user.Email,
+          HashedPassword: user.HashedPassword,
+          UserName: user.UserName
+        },
+        message: `USER CREATED SUCCESFULLY`,
+        proceed: true,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.send("ERROR CREATING ACCOUNT");
+    }
+  });  
+
   const start = async () => {
     try {
       await mongoose.connect(
-        "mongodb+srv://ndabasteve1:dogimoto@test.wgtg2tr.mongodb.net/test?retryWrites=true&w=majority"
-      );
+        "mongodb+srv://ndabasteve1:dogimoto@test.4jc1vgd.mongodb.net/?retryWrites=true&w=majority" );
       app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`);
       });
@@ -60,5 +100,7 @@ app.post("/SignIn", async (req, res) => {
       res.status(500).send(e.message);
     }
   };
+
+
   
   start();
